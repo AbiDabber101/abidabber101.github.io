129ac54d7ba20870d2e85d033e358cd68dd573f6
AbiDabber101 <abinash.sornan@dcmail.ca>
2026-04-10 15:28:38 -0400

    Update main.js
    Created Shape superclass for moving objects

--- lab4/part4/main.js
+++ lab4/part4/main.js
@@ -1,15 +1,32 @@
+// set up canvas
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
+// function to generate random number
+
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// function to generate random RGB color value
+
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+class Shape {
+
+  constructor(x, y, velX, velY) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+  }
+
 }
 
 class Ball {
@@ -33,9 +50,11 @@ class Ball {
     if (this.x + this.size >= width) {
       this.velX = -Math.abs(this.velX);
     }
+
     if (this.x - this.size <= 0) {
       this.velX = Math.abs(this.velX);
     }
+
     if (this.y + this.size >= height) {
       this.velY = -Math.abs(this.velY);
     }
@@ -95,26 +114,3 @@ function loop() {
 }
 
 loop();
-
-const balls = [];
-
-while (balls.length < 25) {
-    const size = random(10, 20);
-    const ball = new Ball(
-    random(0 + size, width - size),
-    random(0 + size, height - size),
-    random(-7, 7),
-    random(-7, 7),
-    randomRGB(),
-    size
-  );
-  balls.push(ball);
-  count++;
-  para.textContent = 'Ball count: ' + count;
-}
-
-const evilBall = new EvilCircle(random(0, width), random(0, height));
-
-function loop() {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
-  ctx.fillRect(0, 0, width, height);
