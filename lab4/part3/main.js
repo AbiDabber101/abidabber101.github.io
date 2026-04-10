edfa7c1e3988e3179458d92168742531d1344bda
AbiDabber101 <abinash.sornan@dcmail.ca>
2026-04-09 23:37:23 -0400

    updated main.js
    creates function to generate random number

--- lab4/part3/main.js
+++ lab4/part3/main.js
@@ -3,3 +3,7 @@ const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
